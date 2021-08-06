5abe7e0cda3533e2e19368e8ca6b142a43aaba4f
R Williams <rjhockey1313@gmail.com>
2021-08-06 12:37:10 -0400

    Multi enemies, multi spawn
    After 20 or so enemies spawn, they no longer are visible despite showing they spawn in hierarchy during game play.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,12 +1,13 @@
+using System;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     float _nextSpawnTime;
 
-    [SerializeField] float _delay = 2f;
-    [SerializeField] GameObject _prefab;
+    [SerializeField] float _delay = 2f; 
     [SerializeField] Transform[] _spawnPoints;
+    [SerializeField] Enemy[] _enemies;
 
     // Update is called once per frame
     void Update()
@@ -19,9 +20,25 @@ public class Spawner : MonoBehaviour
     {
         _nextSpawnTime = Time.time + _delay;
 
+        Transform spawnPoint = ChooseSpawnPoint();
+        Enemy enemy = ChooseEnemy();
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        if (!enemy.isActiveAndEnabled)
+            enemy.enabled = true;
+    }
+
+    Enemy ChooseEnemy()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, _enemies.Length);
+        var enemy = _enemies[randomIndex];
+        return enemy;
+    }
+
+    Transform ChooseSpawnPoint()
+    {
         int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.Length);
         var spawnPoint = _spawnPoints[randomIndex];
-        Instantiate(_prefab, spawnPoint.position, spawnPoint.rotation);
+        return spawnPoint;
     }
 
     bool ShouldSpawn()
