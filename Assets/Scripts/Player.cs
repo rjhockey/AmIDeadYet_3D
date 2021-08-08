29dce114dc9aa3d9968e1c9c73514961800a8ec8
R Williams <rjhockey1313@gmail.com>
2021-08-08 16:36:02 -0400

    Player idle
    Add Player idle shooting animation from animo. Update Player Animator with Run parameter to toggle between idle & running animations. Update player script to eval if player is moving to determine animator state.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,13 @@ public class Player : MonoBehaviour
     [SerializeField] float _speed = 1f;
     [SerializeField] Transform _direction;
 
+    Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -15,5 +22,8 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
         // moves player relative to his orientation
         transform.Translate (movement * Time.deltaTime * _speed, _direction);
+
+        // when moving, run will be set to true > animator
+        _animator.SetBool("Run", movement.magnitude > 0);
     }
 }
